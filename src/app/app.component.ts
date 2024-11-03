e0603615a7e4a391d99fa21ee7ef7c49a8721788
Chelsea Wright <chelsea.wr@gmail.com>
2024-11-02 20:47:07 -0400

    basic grey box - start redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,27 @@ import { faFilePdf } from '@fortawesome/pro-solid-svg-icons';
 import { faChevronDoubleRight } from '@fortawesome/pro-solid-svg-icons';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BoxComponent } from '../box/box.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatTooltipModule, MatButtonModule, FontAwesomeModule],
-  templateUrl: './app.component.html',
+  imports: [
+    RouterOutlet,
+    MatTooltipModule,
+    MatButtonModule,
+    FontAwesomeModule,
+    BoxComponent,
+  ],
+  template: `
+    <div>
+      <h1 id="hello">Hello World, my name is</h1>
+      <h2 id="name">Chelsea May.</h2>
+      <box />
+    </div>
+  `,
   styleUrl: './app.component.scss',
 })
 // break out card at minimum
 export class AppComponent {
   title = 'cdev_ng';
-  faLinkedin = faLinkedin;
-  faGithub = faGithub;
-  faFilePdf = faFilePdf;
-  faChevronDoubleRight = faChevronDoubleRight;
 }
---
+++ src/box/box.component.ts
@@ -0,0 +1,21 @@
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFilePdf } from '@fortawesome/pro-solid-svg-icons';
+import { faChevronDoubleRight } from '@fortawesome/pro-solid-svg-icons';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AdjScrollbar } from '../adjScollbar/adjScrollbar.component';
+
+@Component({
+  selector: 'box',
+  standalone: true,
+  imports: [MatButtonModule, FontAwesomeModule, AdjScrollbar],
+  templateUrl: './box.component.html',
+})
+export class BoxComponent {
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faFilePdf = faFilePdf;
+  faChevronDoubleRight = faChevronDoubleRight;
+}
