1e84a999f4e23963a56a7e43a76358334f5c0874
Chelsea Wright <chelsea.wr@gmail.com>
2024-11-03 20:52:01 -0500

    breakpoint basics, font troubleshooting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
-
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BoxComponent } from '../box/box.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,17 +16,27 @@ import { BoxComponent } from '../box/box.component';
     FontAwesomeModule,
     BoxComponent,
   ],
-  template: ` <div id="card">
-      <h1 id="hello">Hello World, my name is</h1>
-      <h2 id="name">Chelsea May.</h2>
-      <box />
-    </div>
-    <div id="gradient"></div>`,
+  templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-// break out card at minimum
+
+// TODO
+// download pdf
+// swap amp repo
+// animate  mobile page
 export class AppComponent {
   shiningRed = `rgb(152, 31, 36)`;
   shiningOrange = `rgb(223, 95, 24)`;
   title = 'cdev_ng';
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe([Breakpoints.HandsetPortrait])
+      .subscribe((result) => {
+        if (result.matches) {
+          // TODO utilize for fancy mobile scroll animation
+          console.log('result matches mobile');
+        }
+      });
+  }
 }
